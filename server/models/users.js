2e2fea8d4466d9aa6cf0d829ca7bffdd19d40b26
zcallan <kaln.delbridge@hotmail.com>
2017-03-14 09:09:44 +1100

    [untested] user password are now encrypted before being saved

--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require( 'mongoose' );
+const bcrypt = require( 'bcrypt' );
 const Schema = mongoose.Schema;
 
 
@@ -29,8 +30,37 @@ const UsersSchema = new Schema({
 });
 
 UsersSchema.pre( 'save', next => {
+  /* Concatenate first and last name for convenience. */
   this.name.full = `${this.name.first} ${this.name.last}`;
+
+  /* Store an encrypted copy of the user password. */
+  if ( this.isModified( 'password' )) {
+    /* Generate the salt to use in the password hash. */
+    bcrypt.genSalt( 5, ( err, salt ) => {
+      if ( err )
+        return next( err );
+
+      /* Hash the password using the salt and save it to the document. */
+      bcrypt.hash( this.password, salt, null, ( err, hash ) => {
+        if ( err )
+          return next( err );
+
+        this.password = hash;
+        next();
+      });
+    });
+  }
+
   next();
 });
+
+UsersSchema.methods.verifyPassword = ( password, next ) => {
+  bcrypt.compare( password, this.password, ( err, match ) => {
+    if ( err )
+      return next( err );
+
+    next( null, match );
+  });
+};
 
 module.exports = mongoose.model( 'Users', UsersSchema );
