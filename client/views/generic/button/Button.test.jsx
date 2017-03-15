0de20d528dd586d700dc0a7f03b7eac4d036642e
matthaywardwebdesign <matt@matthayward.com.au>
2017-03-15 15:23:01 +1100

    Updated button tests

--- client/views/generic/button/Button.test.jsx
+++ client/views/generic/button/Button.test.jsx
@@ -10,3 +10,19 @@ test( 'Text displaying properly', () => {
   const tree = component.toJSON();
   expect( tree.children[0].children[0] ).toEqual( 'Test Button Text' );
 });
+
+test( 'Selected class is showing correctly', () => {
+  const component = renderer.create(
+    <Button selected type="default">Test Button Text</Button>
+  );
+  const tree = component.toJSON();
+  expect( tree.props.className ).toContain( 'selected' );
+});
+
+test( 'Class name is being passed in correctly', () => {
+  const component = renderer.create(
+    <Button className="test-class" type="default">Test Button Text</Button>
+  );
+  const tree = component.toJSON();
+  expect( tree.props.className ).toContain( 'test-class' );
+});
