4a21e878aa95e9d2b4af35f79ae28348f0b1fe4d
zcallan <kaln.delbridge@hotmail.com>
2017-03-20 23:33:43 +1100

    login works with simple error handling

--- client/views/app/login/Login.jsx
+++ client/views/app/login/Login.jsx
@@ -4,39 +4,47 @@ import { Redirect } from 'react-router-dom';
 import { Container, Input, Button, ButtonGroup, Form } from 'views/generic';
 import API from 'utils/api/api.js';
 
+
 class Login extends Component {
   state = {
     loggingIn: false,
     authenticated: this.props.user.authenticated,
+    error: null,
   }
 
   componentWillReceiveProps( nextProps ) {
-    this.setState({ authenticated: nextProps.user.authenticated });
+    if ( nextProps.user.authenticated !== this.state.authenticated ) {
+      this.setState({ authenticated: nextProps.user.authenticated });
+    }
   }
 
   handleSubmit = ( event, data ) => {
     this.setState({ loggingIn: true });
-    this.props.userAuthenticating();
-    API.authenticate( data ).then( success => {
-      console.log( success );
-    }).catch( error => {
-      console.log( 'An error has occured' );
-    });
+
+    API.authenticate( data )
+      .then(({ data }) => {
+        this.setState({ loggingIn: false });
+        this.props.userAuthenticated( data );
+      })
+      .catch(({ response })  => {
+        this.setState({ error: response.data.error, loggingIn: false });
+      });
   }
 
   render() {
     const { from } = this.props.location.state || { from: { pathname: '/' }};
-    const { authenticated } = this.props.user;
-    const { loggingIn } = this.state;
+    const { loggingIn, authenticated, error } = this.state;
 
     /* Redirect the user to either their previous location (if from a private route), or to home. */
     if ( authenticated ) {
       return <Redirect to={from} />;
     }
 
+    console.log( error );
     return (
       <Container className="login">
         <h2>Login</h2>
+        {( error ) && <h5 className="login-error">{error}</h5>}
         <Form onSubmit={this.handleSubmit}>
           <ButtonGroup>
             <Button type="default" icon="person" column>Customer</Button>
