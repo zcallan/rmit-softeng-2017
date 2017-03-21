916f8480f6680305aa4cb91159c661790430ba3c
matthaywardwebdesign <matt@matthayward.com.au>
2017-03-21 12:37:55 +1100

    Added individual employee endpoints and tests

--- server/routes/employee/get.test.js
+++ server/routes/employee/get.test.js
@@ -4,15 +4,27 @@ const config = require( '../../config/config.json' );
 const client = request.createClient( config.test.url );
 
 describe('GET /employee', () => {
+  before(done => {
+    client.post('/employee', {
+      firstName: 'Bob',
+      lastName: 'Jones',
+      email: 'test@test.com',
+      password: 'hunter2',
+    }, done);
+  });
+
+  after( done => {
+    client.delete('/user/test@test.com', {}, () => {
+      done();
+    });
+  });
+
   it('should return a list of employees', done => {
     client.get('/employee', ( err, res, body ) => {
       chai.expect(body).to.be.instanceof(Array);
-
-      if ( body.length > 0 ) {
-        chai.expect(body[0]).to.have.property('email');
-        chai.expect(body[0]).to.have.property('name');
-        chai.expect(body[0].type).to.equal('employee');
-      }
+      chai.expect(body[0]).to.have.property('email');
+      chai.expect(body[0]).to.have.property('name');
+      chai.expect(body[0].type).to.equal('employee');
       done();
     });
   });
---
+++ server/routes/employee/id/get.js
@@ -0,0 +1,21 @@
+const HttpStatus = require('http-status-codes');
+const User = require('../../../models/user.js');
+
+
+module.exports = ( req, res ) => {
+  const email = req.params.id;
+  User.findOne({ email, type: 'employee' }, ( err, user ) => {
+    /* If an error occurred, return the error */
+    if ( err ) {
+      res.status( HttpStatus.INTERNAL_SERVER_ERROR );
+      return res.json({ error: err });
+    }
+
+    if ( !user ) {
+      res.status( HttpStatus.NOT_FOUND );
+      return res.json({ error: 'The user could not be found' });
+    }
+
+    return res.json(user.toObject());
+  });
+};
