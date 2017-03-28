5170c770fd6b1d1bbd1cf5542b8065df606b4e93
matthaywardwebdesign <matt@matthayward.com.au>
2017-03-28 11:02:22 +1100

    Updated indentation on some files

--- client/views/employee/employee.reducer.js
+++ client/views/employee/employee.reducer.js
@@ -43,56 +43,56 @@ const initialState = {
 
 export default ( state = initialState, { type, payload }) => {
   switch ( type ) {
-    case 'REQUESTED_EMPLOYEE':
-      return {
-        ...state,
-        fetching: true,
-        error: null,
-      };
+  case 'REQUESTED_EMPLOYEE':
+    return {
+      ...state,
+      fetching: true,
+      error: null,
+    };
 
-    case 'RECEIVED_EMPLOYEE':
+  case 'RECEIVED_EMPLOYEE':
+    return {
+      ...state,
+      fetching: false,
+      list: [
+        ...state.list,
+        payload,
+      ],
+      error: null,
+    };
+
+  case 'REQUESTED_EMPLOYEES':
+    return {
+      ...state,
+      fetching: true,
+      error: null,
+    };
+
+  case 'RECEIVED_EMPLOYEES':
+    return {
+      ...state,
+      fetching: true,
+      list: payload,
+      error: null,
+    };
+
+  case 'UPDATED_EMPLOYEE_SCHEDULE': {
+    const employee = Object.assign( {}, state.list.find( empl => empl.id === payload.employeeId ) );
+
+    if ( employee ) {
+      employee.schedule[payload.day] = payload.schedule;
       return {
         ...state,
-        fetching: false,
         list: [
           ...state.list,
-          payload,
         ],
-        error: null,
       };
-
-    case 'REQUESTED_EMPLOYEES':
-      return {
-        ...state,
-        fetching: true,
-        error: null,
-      };
-
-    case 'RECEIVED_EMPLOYEES':
-      return {
-        ...state,
-        fetching: true,
-        list: payload,
-        error: null,
-      };
-
-    case 'UPDATED_EMPLOYEE_SCHEDULE': {
-      const employee = Object.assign( {}, state.list.find( empl => empl.id === payload.employeeId ) );
-
-      if ( employee ) {
-        employee.schedule[payload.day] = payload.schedule;
-        return {
-          ...state,
-          list: [
-            ...state.list,
-          ],
-        };
-      } else {
-        return state;
-      }
-    }
-
-    default:
+    } else {
       return state;
+    }
+  }
+
+  default:
+    return state;
   }
 };
