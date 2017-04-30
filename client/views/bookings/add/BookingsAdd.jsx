337c53ca06c4bf0b3d9cbb5d938ded4bf1dc70ad
zcallan <kaln.delbridge@hotmail.com>
2017-05-01 01:02:00 +1000

    updated admin booking to new booking mechanics

--- client/views/bookings/add/BookingsAdd.jsx
+++ client/views/bookings/add/BookingsAdd.jsx
@@ -12,33 +12,59 @@ class BookingsAdd extends Component {
   componentDidMount() {
     document.title = `Add Booking | ${config.companyName}`;
     this.props.setPageTitle( 'Add Booking', 'You can add a customer booking on this page' );
+    this.props.requestedEmployees();
+    this.props.requestActivities();
 
-    API.getCustomers().then(({ data }) => {
-      this.props.receivedCustomers( data );
-    }).catch(() => {
-      this.props.fetchCustomersFail();
-    });
+    API.getEmployees()
+      .then( this.props.receivedEmployees )
+      .catch( this.props.fetchEmployeesFail );
 
-    API.getEmployees().then(({ data }) => {
-      this.props.receivedEmployees( data );
-    }).catch(() => {
-      this.props.fetchEmployeesFail();
-    });
+    API.getActivities()
+      .then( this.props.receiveActivities )
+      .catch( this.props.fetchActivitiesFail );
+
+    API.getCustomers()
+      .then(({ data }) => this.props.receivedCustomers( data ))
+      .catch( this.props.fetchCustomersFail );
   }
 
   state = {
     error: null,
     success: null,
+    availabilities: {
+      data: [],
+      loading: false,
+      error: null,
+    },
+    selectedDay: null,
+    selectedActivity: null,
+    selectedCustomer: null,
     time: {
       start: moment().startOf( 'day' ).valueOf(),
       end: moment().startOf( 'day' ).valueOf(),
-    }
+    },
+    activities: {
+      list: [
+        {
+          name: 'Haircut',
+          duration: 30,
+        },
+        {
+          name: 'Handjob',
+          duration: 2,
+        }
+      ],
+      loading: false,
+      error: null,
+    },
   }
 
   handleSubmit = event => {
     event.preventDefault();
 
     const data = serialize( this.form, { hash: true });
+    data.customer = this.props.user.data.email;
+    data.activity = this.state.selectedActivity;
     this.setState({ success: null, error: null });
     API.createBooking( data ).then(() => {
       this.setState({ success: 'Booking successfully created' });
@@ -47,74 +73,189 @@ class BookingsAdd extends Component {
     });
   }
 
+  handleTimeUpdate = time => {
+    this.setState({ time });
+  }
+
+  handleSelectEmployee = event => {
+    const { value } = event.target;
+
+    this.setState( state => ({
+      selectedDay: null,
+      time: {
+        start: moment().startOf( 'day' ).valueOf(),
+        end: moment().startOf( 'day' ).valueOf(),
+      },
+      availabilities: {
+        ...state.availabilities,
+        loading: true,
+        error: null,
+      }
+    }));
+
+    API.getEmployeeAvailabilities( value )
+      .then( success => this.setState({
+        selectedDay: success.data.length > 0 && success.data[0].dayOfWeek,
+        availabilities: {
+          loading: false,
+          error: null,
+          data: success.data,
+        }
+      }))
+      .catch( error => this.setState({
+        availabilities: {
+          loading: false,
+          data: [],
+          error,
+        }
+      }));
+  }
+
+  handleSelectDay = event => {
+    const { value } = event.target;
+
+    this.setState({ selectedDay: value });
+  }
+
+  handleSelectActivity = event => {
+    const { value } = event.target;
+    this.setState({ selectedActivity: JSON.parse(value) });
+  }
+
+  handleSelectCustomer = event => {
+    const { value } = event.target;
+    this.setState({ selectedCustomer: value });
+  }
+
+  renderHours() {
+    const { time, selectedDay, selectedActivity } = this.state;
+    const { data } = this.state.availabilities;
+
+    /* Get an array of the available times the employee has on the selected day. */
+    const allowedTimes = [... new Set( data
+      .filter( time => time.dayOfWeek === selectedDay )
+      .map( time => ({
+        start: time.start - 1,
+        end: time.end + 1,
+      }))
+    )];
+
+    const activityDuration = selectedActivity ? parseInt( selectedActivity.duration, 10 ) : 30;
+
+    return (
+      <TimePair time={time} minDuration={activityDuration}>
+        {( start, end, onChange ) => (
+          <div>
+            <label htmlFor="start">Start Time</label>
+            <Input
+              type="time"
+              name="start"
+              required
+              onChange={value => onChange( 'start', value, this.handleTimeUpdate )}
+              value={start}
+              step={activityDuration}
+              browserDefault
+              allowedTimes={allowedTimes}
+            />
+
+            <label htmlFor="end">End Time</label>
+            <Input
+              type="time"
+              name="end"
+              required
+              onChange={value => onChange( 'end', value, this.handleTimeUpdate )}
+              value={end}
+              step={activityDuration}
+              browserDefault
+              allowedTimes={allowedTimes}
+            />
+          </div>
+        )}
+      </TimePair>
+    );
+  }
+
+  renderDays() {
+    const { data } = this.state.availabilities;
+    const days = [...new Set( data.map( time => time.dayOfWeek ))];
+
+    return (
+      <div>
+        <label htmlFor="dayOfWeek">Day</label>
+        <select name="dayOfWeek" onChange={this.handleSelectDay} value={this.state.selectedDay}>
+          <option disabled>Choose a day</option>
+          {days.map( d => <option key={d} value={d.toLowerCase()}>{d}</option>)}
+        </select>
+      </div>
+    );
+  }
+
   render() {
-    const { customers, employees } = this.props;
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const { employees, activities, customers } = this.props;
+    const { availabilities, selectedActivity, selectedCustomer } = this.state;
 
     return (
       <Row center>
         <Col sm={6}>
-          { this.state.error && <Error>{this.state.error}</Error> }
-          { this.state.success && <Success>{this.state.success}</Success> }
+          {this.state.error && <Error>{this.state.error}</Error>}
+          {this.state.success && <Success>{this.state.success}</Success>}
           <div className="bookings-add">
             <i className="material-icons">playlist_add</i>
             <h3>Add Booking</h3>
 
             <form onSubmit={this.handleSubmit} ref={form => this.form = form}>
               <label htmlFor="customer">Customer</label>
-              <select name="customer">
-                {( customers.list && customers.list.length > 0 ) ? customers.list.map( customer => (
-                    <option value={customer.email}>{customer.name.full}</option>
-                  )) : ( customers.fetching ) ? (
-                      <option>Loading...</option>
-                    ) : (
-                      <option>No customers found</option>
-                    )}
-              </select>
-
-              <label htmlFor="employee">Employee</label>
-              <select name="employee">
-                {( employees.list && employees.list.length > 0 ) ? employees.list.map( customer => (
-                    <option value={customer.email}>{customer.name.full}</option>
-                  )) : ( employees.fetching ) ? (
-                      <option>Loading...</option>
-                    ) : (
-                      <option>No employees found</option>
-                    )}
-              </select>
-
-              <label htmlFor="dayOfWeek">Day</label>
-              <select name="dayOfWeek">
-                {days.map( d => <option key={d} value={d.toLowerCase()}>{d}</option>)}
-              </select>
-
-              <TimePair time={this.state.time}>
-                {( start, end, onChange ) => (
-                  <div>
-                    <label htmlFor="start">Start Time</label>
-                    <Input
-                      type="time"
-                      name="start"
-                      required
-                      onChange={value => onChange( 'start', value, updatedTime => this.setState({ time: updatedTime }))}
-                      value={start}
-                      step={30}
-                      browserDefault
-                    />
-
-                    <label htmlFor="end">End Time</label>
-                    <Input
-                      type="time"
-                      name="end"
-                      required
-                      onChange={value => onChange( 'end', value, updatedTime => this.setState({ time: updatedTime }))}
-                      value={end}
-                      step={30}
-                      browserDefault
-                    />
-                  </div>
+              <select name="customer" onChange={this.handleSelectCustomer}>
+                {( customers.list && customers.list.length > 0 ) ? [
+                  <option disabled selected>Choose an activity</option>,
+                  customers.list.map( customer => <option value={customer.email}>{customer.name.full}</option> )
+                ] : ( customers.loading ) ? (
+                  <option disabled selected>Loading...</option>
+                ) : (
+                  <option disabled selected>No customers found</option>
                 )}
-              </TimePair>
+              </select>
+
+              {( selectedCustomer ) && (
+                <div>
+                  <label htmlFor="activity">Activity</label>
+                  <select name="activity" onChange={this.handleSelectActivity}>
+                    {( activities.list && activities.list.length > 0 ) ? [
+                      <option disabled selected>Choose an activity</option>,
+                      activities.list.map( activity => <option value={JSON.stringify(activity)}>
+                        {activity.name} ({activity.duration} minutes)
+                      </option> )
+                    ] : ( activities.loading ) ? (
+                      <option disabled selected>Loading...</option>
+                    ) : (
+                      <option disabled selected>No activities found</option>
+                    )}
+                  </select>
+
+                  {( selectedActivity ) && (
+                    <div>
+                      <label htmlFor="employee">Employee</label>
+                      <select name="employee" onChange={this.handleSelectEmployee}>
+                        {( employees.list && employees.list.length > 0 ) ? [
+                          <option disabled selected>Choose an employee</option>,
+                          employees.list.map( customer => <option value={customer.email}>{customer.name.full}</option>
+                        )] : ( employees.fetching ) ? (
+                          <option disabled selected>Loading...</option>
+                        ) : (
+                          <option disabled selected>No employees found</option>
+                        )}
+                      </select>
+
+                      {( availabilities.loading ) && <p>Loading...</p>}
+
+                      {( availabilities.data.length > 0 ) && [
+                        this.renderDays(),
+                        this.renderHours(),
+                      ]}
+                    </div>
+                  )}
+                </div>
+              )}
               <Button type="default" success text="Create Booking" icon="add" submit />
             </form>
           </div>
--- client/views/bookings/add/bookingsAdd.container.js
+++ client/views/bookings/add/bookingsAdd.container.js
@@ -3,16 +3,31 @@ import { bindActionCreators } from 'redux';
 import BookingsAdd from './BookingsAdd.jsx';
 import { requestedCustomers, receivedCustomers, fetchCustomersFail } from 'views/customer/customer.actions.js';
 import { requestedEmployees, receivedEmployees, fetchEmployeesFail } from 'views/employee/employee.actions.js';
+import { requestActivities, receiveActivities, fetchActivitiesFail } from 'views/activities/activities.actions.js';
 import { setPageTitle } from 'views/generic/page-title/pageTitle.actions.js';
 
 
 const mapStateToProps = state => ({
   customers: state.customers,
   employees: state.employees,
+  user: state.user,
+  activities: state.activities,
 });
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ requestedCustomers, receivedCustomers, requestedEmployees, receivedEmployees, fetchEmployeesFail, fetchCustomersFail, setPageTitle }, dispatch );
+  return bindActionCreators({
+    requestedCustomers,
+    receivedCustomers,
+    requestedEmployees,
+    receivedEmployees,
+    fetchEmployeesFail,
+    fetchCustomersFail,
+    setPageTitle,
+    receiveActivities,
+    fetchActivitiesFail,
+    requestActivities,
+  }, dispatch );
 };
 
 export default connect( mapStateToProps, mapDispatchToProps )( BookingsAdd );
+
