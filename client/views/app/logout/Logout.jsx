1155ea0932e171f743daa3e8cc48265fb5e756e0
matthaywardwebdesign <matt@matthayward.com.au>
2017-05-01 00:27:08 +1000

    Adding comments to logout view

--- client/views/app/logout/Logout.jsx
+++ client/views/app/logout/Logout.jsx
@@ -1,34 +1,44 @@
+/* This file provides the logout view of the application */
+
+
+/* Import any dependencies */
 import './logout.scss';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Container } from 'views/generic';
 
-
 class Logout extends Component {
+  /* Set the inital state of the component */
   state = {
     authenticated: this.props.user.authenticated,
   }
 
+  /* This function gets called when the component loads / mounts */
   componentDidMount() {
     /* Remove the auth information from localStorage */
     localStorage.removeItem( 'auth' );
 
+    /* Tell the parent component that the user has logged out */
     this.props.userLogout();
   }
 
+  /* If the users authentication status updates we need to update the components internal state */
   componentWillReceiveProps( nextProps ) {
     if ( nextProps.user.authenticated !== this.state.authenticated ) {
       this.setState({ authenticated: nextProps.user.authenticated });
     }
   }
 
+  /* This function provides the rendering of this component */
   render() {
     const { authenticated } = this.state;
 
+    /* If the user isn't authenticated redirect to the homepage */
     if ( !authenticated ) {
       return <Redirect to="/" />;
     }
 
+    /* Render the logout message */
     return (
       <Container className="logout">
         <h3>Logging you out, please wait...</h3>
--- client/views/app/logout/logout.container.js
+++ client/views/app/logout/logout.container.js
@@ -1,13 +1,17 @@
+/* This file binds the state ( data store ) to the view */
+
+/* Import dependencies */
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Logout from './Logout.jsx';
 import { userLogout } from 'views/user/user.actions.js';
 
-
+/* We wish to access the user data in this view so we map it here */
 const mapStateToProps = state => ({
   user: state.user,
 });
 
+/* Map any actions that the view may perform here */
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({ userLogout }, dispatch );
 };
