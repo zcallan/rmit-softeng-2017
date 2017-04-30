45264e9e6b5024c7d5d8deffc304e6607f4c5b57
matthaywardwebdesign <matt@matthayward.com.au>
2017-05-01 00:23:00 +1000

    Adding comments to files

--- client/views/app/login/Login.jsx
+++ client/views/app/login/Login.jsx
@@ -1,3 +1,6 @@
+/* This file provides the Login view of the application */
+
+/* Require dependencies */
 import './login.scss';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
@@ -6,40 +9,51 @@ import API from 'utils/api/api.js';
 import config from 'config/branding.json';
 
 class Login extends Component {
+  /* Set the initial state of this component */
   state = {
     loggingIn: false,
     authenticated: this.props.user.authenticated,
     error: null,
   }
 
+  /* When the component mounts / loads set the page title */
   componentDidMount() {
     document.title = `Login | ${config.companyName}`;
     this.props.setPageTitle( 'Login', 'You can login on this page using your username and password' );
   }
 
+  /* Check whether the user has authenticated and update this components state */
   componentWillReceiveProps( nextProps ) {
     if ( nextProps.user.authenticated !== this.state.authenticated ) {
       this.setState({ authenticated: nextProps.user.authenticated });
     }
   }
 
+  /* This function handles the submission of the login form */
   handleSubmit = ( event, data ) => {
+    /* Update the state of the component to be loggingIn */
     this.setState({ loggingIn: true });
 
+    /* Make an authentication request to the API using the login credentials */
     API.authenticate( data )
       .then(({ data }) => {
+        /* Update the state of the component */
         this.setState({ loggingIn: false });
         /* Store the user information and token in local storage */
         localStorage.setItem( 'auth', JSON.stringify( data ));
         this.props.userAuthenticated( data );
+        /* Update the access token the API client / library uses */
         API.setToken( data.token );
       })
       .catch(({ response })  => {
+        /* There was an error logging it, store the error in the state of the component */
         this.setState({ error: response.data.error, loggingIn: false });
       });
   }
 
+  /* This function provides the rendering of this component */
   render() {
+    /* Get the previous page that was loaded so we can redirect to it once logged in */
     const { from } = this.props.location.state || { from: { pathname: '/' }};
     const { loggingIn, authenticated, error } = this.state;
 
@@ -48,6 +62,7 @@ class Login extends Component {
       return <Redirect to={from} />;
     }
 
+    /* Render the login form and any errors that may have occured */
     return (
       <Container className="login">
         {( error ) && <Error>{error}</Error>}
--- client/views/app/login/login.container.js
+++ client/views/app/login/login.container.js
@@ -1,13 +1,18 @@
+/* This file binds the state ( data store ) to the view */
+
+/* Import dependencies */
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Login from './Login.jsx';
 import { userAuthenticated } from 'views/user/user.actions';
 import { setPageTitle } from 'views/generic/page-title/pageTitle.actions.js';
 
+/* We wish to access the user data in this view so we map it here */
 const mapStateToProps = state => ({
   user: state.user,
 });
 
+/* Map any actions that the view may perform here */
 function mapDispatchToProps( dispatch ) {
   return bindActionCreators({ userAuthenticated, setPageTitle }, dispatch );
 }
